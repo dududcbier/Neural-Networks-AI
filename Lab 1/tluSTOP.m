15cfe15c706327601fc94f1f56fc22d4f7926ac7
Eduardo Bier <dududcbier@gmail.com>
2016-02-17 12:35:10 +0100

    Lab1

---
+++ Lab 1/tluSTOP.m
@@ -0,0 +1,94 @@
+% TLU implementation
+% Eduardo Delgado Coloma Bier 
+% Mikel Orbea Sotil
+
+% Parameters
+learn_rate = 0.1;    % the learning rate
+n_epochs = 100;      % the number of epochs we want to train
+
+% Define the inputs
+examples = [0 0; 0 1; 1 0; 1 1];
+
+% Define the corresponding target outputs
+goal = [0 0 0 1];
+
+% Initialize the weights and the threshold
+weights = [rand rand];
+threshold = rand;
+
+% Preallocate vectors for efficiency. They are used to log your data 
+% The 'h' is for history
+h_error = zeros(n_epochs,1);
+h_weights = zeros(n_epochs,2);
+h_threshold = zeros(n_epochs,1);
+
+% Store number of examples and number of inputs per example
+n_examples = size(examples,1);     % The number of input patterns
+n_inputs = size(examples,2);       % The number of inputs
+
+epoch = 1;
+stop = 1;
+while (epoch <= n_epochs && stop == 1)
+    stop = 1;
+    epoch_error = zeros(n_examples,1);
+    
+    h_weights(epoch,:) = weights;
+    h_threshold(epoch) = threshold;
+    
+    for pattern = 1:n_examples
+        
+        % Initialize weighted sum of inputs
+        summed_input = weights(1)* examples(pattern, 1) + weights(2) * examples(pattern, 2);
+        
+        % Subtract threshold from weighted sum
+        threshold_value = summed_input - threshold;
+        
+        % Compute output
+        if threshold_value >= 0
+            output = 1;
+        else
+            output = 0;
+        end
+        
+        % Compute error
+        error = (goal(pattern) - output);
+        if (error ~= 0) 
+            stop = 0;
+        end
+         
+        
+        % Compute delta rule
+        delta_weights = learn_rate * error * examples(pattern,:);
+        delta_threshold = - learn_rate * error;
+        
+        % Update weights and threshold
+        weights = weights + delta_weights ;
+        threshold = threshold + delta_threshold;        
+    
+        % Store squared error
+        epoch_error(pattern) = error.^2;
+    end
+    
+    h_error(epoch) = sum(epoch_error);
+    epoch = epoch + 1;
+end
+
+n_epochs = epoch;
+
+% Plot functions
+figure(1);
+plot(h_error(1:n_epochs))
+title('\textbf{TLU-error over epochs}', 'interpreter', 'latex', 'fontsize', 12);
+xlabel('\# of epochs', 'interpreter', 'latex', 'fontsize', 12)
+ylabel('Summed Squared Error', 'interpreter', 'latex', 'fontsize', 12)
+
+figure(2);
+plot(1:n_epochs,h_weights(1:n_epochs,1),'r-','DisplayName','weight 1')
+hold on
+plot(1:n_epochs,h_weights(1:n_epochs,2),'b-','DisplayName','weight 2')
+plot(1:n_epochs,h_threshold(1:n_epochs),'k-','DisplayName','threshold')
+xlabel('\# of epochs', 'interpreter', 'latex', 'fontsize', 12)
+title('\textbf{Weight vector and threshold vs epochs}', 'interpreter', 'latex', 'fontsize', 12);
+h = legend('location','NorthEast');
+set(h, 'interpreter', 'latex', 'fontsize', 12);
+hold off
